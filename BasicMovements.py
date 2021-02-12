9946aa30b8be620e9fb60c818118f57d0714c03d
Erik Shrestha <Eshrestha@student.neumont.edu>
2021-02-11 17:15:26 -0700

    Got drone moving

--- BasicMovements.py
+++ BasicMovements.py
@@ -1,1 +1,13 @@
-print("hello")
+from djitellopy import tello
+from time import sleep
+
+drone = tello.Tello()
+drone.connect()
+print(drone.get_battery())
+
+
+drone.takeoff()
+drone.send_rc_control(0, 50, 0, 0)
+sleep(2)
+drone.send_rc_control(0, 0, 0, 0)
+drone.land()
